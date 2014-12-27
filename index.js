efde7d6ed0d9de05acebf2deb9088396e8dfabc5
J Letts <entrepreneurj@gmail.com>
2014-12-27 10:37:32 -0500

    Adds tests for game module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require("http")
 var express = require("express")
 var app = express()
-var port = process.env.PORT || 5001
+var port = process.env.PORT || 5000
 
 // User defined modules
 
@@ -14,9 +14,9 @@ app.use('/library', express.static(__dirname + '/public/sculpt'));
 
 var server = http.createServer(app)
 server.listen(port)
+console.log("http server listening on %d", port)
+
+// Inpspired by: http://stackoverflow.com/questions/13151693/passing-arguments-to-require-when-loading-module
 var sockets = require('./lib/sockets.js')(server)
 
-console.log("http server listening on %d", port)
-// Set up room logic
 console.log("websocket server created")
-
