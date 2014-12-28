09a8b5f1922af5fe963fbe8c66e235693756d3fc
J Letts <entrepreneurj@gmail.com>
2014-12-28 11:39:11 -0500

    Finer detail on socket intialisation test

--- test/sockets.js
+++ test/sockets.js
@@ -1,14 +1,36 @@
-var expect = require("chai").expect;
+"use strict";
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var expect = chai.expect;
+chai.use(sinonChai);
+
 var utils = require("../lib/utils");
 describe("sockets", function() {
 
-	it("should require a server parameter",function() {
+	var Sockets = require("../lib/sockets");
+	describe("initialisation", function() {
+		describe("should require an http server input",function() {
+
+			it("should reject a null input", function() {
+				var Sockets = require("../lib/sockets");
+				expect(Sockets.bind(this, null)).to.throw("`port` or a `server` must be provided");
+			});
+
+			it("should accept a server input", function() {
+        var http = require("http");
+        var server = http.createServer(function(data) {console.log(data);});
+
+        //var spy = sinon.spy(server);
+        //console.log(spy);
+				expect(Sockets.bind(this,server)).to.not.throw();
+        //expect(spy).to.have.been.called();
 
 
-		var Sockets = require("../lib/sockets");
-		expect(Sockets.bind(this, null)).to.throw("`port` or a `server` must be provided");
+			});
 
-
-
+		});
 	});
+
+  
 });
