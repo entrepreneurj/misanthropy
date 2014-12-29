63ece6cc7f7c174d121235880d2d246e4fd4a04b
J Letts <entrepreneurj@gmail.com>
2014-12-29 14:01:46 -0500

    Solves websocket testing
    * Uses ws library to create ws client
    * Uses ws_response array to log message received by ws client
    * Uses sinon.spy to assert against console output
    * Carries out operations first then tests results afterwards

--- test/sockets.js
+++ test/sockets.js
@@ -42,7 +42,11 @@ describe("sockets", function() {
 		// Uses Sinon to spy on console.log
 		var spy;
 		beforeEach(function() {spy = sinon.spy(console,"log");console.log('before every test');});
-		afterEach(function() {console.log.restore(); spy=null;console.log('after every test')});
+		afterEach(function() {
+			console.log.restore();
+			spy=null;
+			console.log('after every test');
+		});
 
 		// Create server
 		var server = http.createServer(function(data){});
@@ -77,85 +81,103 @@ describe("sockets", function() {
 
 		});
 	});
-	describe('client messages', function() {
+	describe('First player joins empty default room', function() {
 
-		var spy=null;
-		// Uses Sinon to spy on console.log
+
+
+		// creates ws_responses array to log messages received by client
+		var ws_responses = [];
+
 		beforeEach(function(done) {
-		 	spy = sinon.spy(console,"log");
-			done();
+			// Uses Sinon to spy on console.log
+			this.spy = sinon.spy(console,"log");
+			this.server = http.createServer(function(data){});
+			this.server.listen(10000);
 
-		});
-		afterEach(function(done) {
-			//console.log(spy);
-			console.log.restore();
-			 spy=null;
-			console.log('after every test');
-			done();
-		});
+			// Create web-socket server
+			this.sockets = Sockets(this.server);
 
-		// Create server
-		var server = http.createServer(function(data){});
-		server.listen(9999);
+			// Create webSocket client
+			this.ws = new WebSocket("ws://localhost:10000");
 
-		// Create web-socket server
-		var sockets = Sockets(server);
-
-
-		describe('should log a message when a new player is added', function(done) {
-			var ws = new WebSocket("ws://localhost:9999");
-			this.timeout(5000);
-			ws.on('open', function() {
-			//	console.log("sending data to server");
-			ws.on('message',function(data, flags){
-
-
-
-				//console.log(spy);
-				//console.log(spy.getCall(2));
-
-
-				// done();
-				var parsed_data = JSON.stringify(data);
-				if (parsed_data.indexOf("joined the game") > -1){
-					it("should log 'new player'", function() {
-					expect(spy.getCall(1).args[0]).to.contain("new_player");
-					});
-					//console.log(data, flags, done);
-					ws.close();
-				}
-
-			});
-				ws.send(
+			// Triggered when client connection opens
+			this.ws.on('open', function() {
+				this.send(
 					JSON.stringify(
 						{'player': "Tom", 'type': "new_player", 'body': {"room":'default'} }
 						)
-				       );
-			//console.log(spy);
-			// onsole.log('open');
-			//
+					);
+
+
 			});
-			//ws.close(1000,"Test purposes", true);
-			//ws.on('close', function() {
 
-		//		done();
+			// Triggered when ws client receives a message.
+			this.ws.on('message', function(data){
 
-		//	});
+				// Add messages received by client to ws_responses array
+				ws_responses.push(JSON.parse(data));
+
+				// We need to close the client after we receive the messages
+				// we're interested in, otherwise the connection stays
+				// open and the test times out...
+				//  - Tests for phrase in message data.
+				if (JSON.stringify(data).indexOf("joined the game")> -1)
+				{
+					this.close();
+				}
+			});
+
+			// Signals that the beforeEach function is done after the
+			// ws client has closed.
+			this.ws.on('close', function() {done();});
+
+		});
+		afterEach(function(done) {
+
+			// Returns functionality to console
+			console.log.restore();
+
+			this.spy=null;
+
+			// Closes simple web server instance
+			this.server.close();
+			// Closes sockets webSockets server as well
+			this.sockets.wss.close();
+			done();
+		});
+
+		it("should return empty player list to the client", function() {
+			// After giving a name, the first thing the client should receive
+			// is a list of current players in the game.
+			// Since the client is the first player in the game, the list
+			// should be empty.
+			expect(ws_responses).to.contain(
+			{ room: 'default', type: 'players', body: [] }
+			);
+
+		});
+
+		it("should have sent the player a message about themselves joining the room", function() {
+				//console.log(ws_responses);
+				//After sending the new player a message about all 0 players
+				// in the room, the server should tell every player in the
+				// room that the new player has joined. This includes
+				// the new player as well.
+
+				expect(ws_responses).to.contain("Tom has joined the game");
+		});
 
 
-			//console.log(console.log.getCall(1));
+		it("should console.log 'new player'", function() {
 
 
+			expect(this.spy.getCall(1).args[0]).to.contain("new_player");
 
 
 		});
-		it("should have sent the player a message about themselves joining the room")//, function() {
-			// Second connection to be sent to conn was new_player data
-			//	expect(dummy_connection.result[1]).to.eql('Tom has joined the game');
-		//});
+
+
 	});
-
-
 
 
 });
