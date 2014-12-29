e5e71521842a540b62cf1357e5c72f4d38225491
J Letts <entrepreneurj@gmail.com>
2014-12-28 23:25:21 -0500

    Adds tests for opening/closing socket connections

--- test/sockets.js
+++ test/sockets.js
@@ -5,7 +5,10 @@ var sinonChai = require("sinon-chai");
 var expect = chai.expect;
 chai.use(sinonChai);
 
+var http = require("http");
 var utils = require("../lib/utils");
+var WebSocket = require("ws");
+
 describe("sockets", function() {
 
 	var Sockets = require("../lib/sockets");
@@ -13,12 +16,12 @@ describe("sockets", function() {
 		describe("should require an http server input",function() {
 
 			it("should reject a null input", function() {
-				var Sockets = require("../lib/sockets");
+
 				expect(Sockets.bind(this, null)).to.throw("`port` or a `server` must be provided");
+
 			});
 
 			it("should accept a server input", function() {
-        var http = require("http");
         var server = http.createServer(function(data) {console.log(data);});
 
         //var spy = sinon.spy(server);
@@ -32,5 +35,47 @@ describe("sockets", function() {
 		});
 	});
 
-  
+	describe("websockets", function() {
+
+		// Uses Sinon to spy on console.log
+		before(function() {sinon.spy(console,"log");});
+		after(function() {console.log.restore();});
+
+		// Create server
+		var server = http.createServer(function(data){});
+		server.listen(9999);
+
+		// Create web-socket server
+		var sockets = Sockets(server);
+
+
+		it("should log to console when a websocket is opened", function() {
+
+		var ws = new WebSocket("ws://localhost:9999");
+		ws.on('open', function() {
+
+			expect(console.log.getCall(1).args[0]).to.eql("websocket connection open");
+
+
+
+		});
+
+
+
+
+		});
+
+		it("should log to console when a websococket is closed", function() {
+
+			var ws = new WebSocket("ws://localhost:9999");
+			ws.on('close', function() {
+
+			expect(console.log.getCall(2).args[0]).to.eql("websocket connection close");
+
+			});
+
+		});
+	});
+
+
 });
